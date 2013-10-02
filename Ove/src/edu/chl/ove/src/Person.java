6eaa28ecdcc2964e3727c75f199866bcd0ac264f
Lisa Stenberg <lisast@student.chalmers.se>
2013-10-02 10:29:27 +0200

    Implemented User

--- Ove/src/edu/chl/ove/src/Person.java
+++ Ove/src/edu/chl/ove/src/Person.java
@@ -9,5 +9,80 @@ package edu.chl.ove.src;
  * @author lisastenberg
  */
 public class Person {
+
+    private int idNumber;
+    private String name;
+    private String mail;
+    private String phoneNbr;
+    private String address;
+    
+    public Person(int idNumber, String name, String mail, String phoneNbr, String address) {
+        this.idNumber = idNumber;
+        this.name = name;
+        this.mail = mail;
+        this.phoneNbr = phoneNbr;
+        this.address = address;
+    }
+
+    public int getIdNumber() {
+        return idNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getPhoneNbr() {
+        return phoneNbr;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    
+    public void setIdNumber(int idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setPhoneNbr(String phoneNbr) {
+        this.phoneNbr = phoneNbr;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + this.idNumber;
+        return hash;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if(o == null) {
+            return false;
+        } else if(this == o) {
+            return true;
+        } else if(this.getClass() != o.getClass()) {
+            return false;
+        } else {
+            Person tmp = (Person)o;
+            return this.getIdNumber() == tmp.getIdNumber();
+        }
+    }
     
 }
--- Ove/src/edu/chl/ove/src/User.java
+++ Ove/src/edu/chl/ove/src/User.java
@@ -4,10 +4,67 @@
  */
 package edu.chl.ove.src;
 
+import java.util.Objects;
+
 /**
  *
  * @author lisastenberg
  */
 public class User {
     
+    private Person person;
+    private String userName;
+    private String passWord;
+    
+    public User(Person person, String userName, String passWord) {
+        this.person = person;
+        this.userName = userName;
+        this.passWord = passWord;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + Objects.hashCode(this.userName);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        if (!Objects.equals(this.userName, other.userName)) {
+            return false;
+        }
+        return true;
+    }
 }
