73addff0aade504288fcdb087090d4d9be455635
skjutar <kristoffer.skjutar@hotmail.com>
2013-10-08 08:41:36 +0200

    first version of GUI

---
+++ OVE/src/main/java/com/mycompany/ove/gui/LoggingBean.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ove.gui;
+
+import java.io.Serializable;
+import javax.ejb.Stateless;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.inject.Named;
+import org.primefaces.context.RequestContext;
+
+/**
+ *
+ * @author kristofferskjutar
+ */
+
+@Named("loggingBean")
+@SessionScoped
+public class LoggingBean implements Serializable{
+  
+    private String username;  
+      
+    private String password;  
+      
+    public String getUsername() {  
+        return username;  
+    }  
+  
+    public void setUsername(String username) {  
+        this.username = username;  
+    }  
+  
+    public String getPassword() {  
+        return password;  
+    }  
+  
+    public void setPassword(String password) {  
+        this.password = password;  
+    }  
+  
+    public void login(ActionEvent actionEvent) {  
+        RequestContext context = RequestContext.getCurrentInstance();  
+        FacesMessage msg = null;  
+        boolean loggedIn = false;  
+          
+        if(username.equals("admin") && password.equals("admin")) {  
+            loggedIn = true;  
+            msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", username);  
+        } else {  
+            loggedIn = false;  
+            msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Login Error", "Invalid credentials");  
+        }  
+          
+        FacesContext.getCurrentInstance().addMessage(null, msg);  
+        context.addCallbackParam("loggedIn", loggedIn);  
+    }  
+}  
+
+
